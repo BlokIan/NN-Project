d821ae11cdcb5bbca1c94670756f6500fe1b0f23
BlokIan <i.d.blok@student.rug.nl>
2024-06-24 12:01:38 +0200

    12 a.m. update
    Ian: CNN architecture
    Brian: visualize numbers, turn data into tensors
    Serban: data split, transform data correct input

--- cnn.py
+++ cnn.py
@@ -1,9 +1,31 @@
 import os
 import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
 from torch import nn
+from torch.nn import ReLU
+from torch.nn import MaxPool2d
+from torch.nn import Conv2d
 from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
 
 class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        
+        self.flatten = nn.Flatten()
+        self.hidden_stack = nn.Sequential(
+            # First layer (W*L*D = (15,16,1))
+            Conv2d(1, 6, 9, padding=1),
+            ReLU(),
+            MaxPool2d(2, stride=2, ceil_mode=True),
+            # After padding (17, 18, 1), convolution (9, 10, 6), pooling (5, 5, 6))
+            # Second layer (W*L*D = (5,5,6))
+            Conv2d()
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        result = self.hidden_stack(x)
+        return result
--- main.py
+++ main.py
@@ -2,34 +2,77 @@ import numpy as np
 import sklearn
 import torch
 import torchvision
+import matplotlib.pyplot as plt
+# import cnn
+
+device = ("cuda")
+print(f"Using {device} device")
+
 
 def get_data(path="mfeat-pix.txt"): 
-    # converts data file to array which contains 10 lists (1 for each number)
+    # converts data file to array which contains 10 arrays (1 for each number)
     # each list contains 200 images
-    # each image consists of 240 pixels
+    # each image is a 15x16 array
     with open(path, "r") as f:
         data = f.read()
     data=data.replace(" ", "")
-    array = []
-    number = []
-    image = []
+    array = np.empty((10, 200), dtype=object)
+    number = np.empty(200, dtype=object)
+    image = np.empty(15 * 16, dtype=int)
+    image_index = 0
+    number_index = 0
+    array_index = 0
     for value in data:
         if value == "\n":
-            number.append(image)
-            image = []
-            if len(number) == 200:
-                array.append(number)
-                number = []
+            image = np.reshape(image, (16, 15))
+            if number.size == 0:
+                number = image
+            else:
+                number[number_index] = image
+                number_index += 1
+            image = np.empty(15 * 16, dtype=int)
+            image_index = 0
+            if number_index == 200:
+                array[array_index] = number
+                array_index += 1
+                number = np.empty(200, dtype=object)
+                number_index = 0
         else:
-            image.append(int(value))
+            image[image_index] = int(value)
+            image_index += 1
     return array
 
-def plot_image():
-    pass
-        
+
+def plot(image):
+    plt.imshow(image, cmap='gray', aspect='auto')
+    plt.title('Grayscale Image')
+    plt.show()
 
 
-x = torch.rand(5, 3)
-print(x)
-print(torch.cuda.is_available())
+if __name__ == "__main__":
+    data = get_data()
+    training_data = data[:, :100]
+    testing_data = data[:, 100:]
 
+    training_data = training_data.reshape([15,16,-1])[:,:,None,:]
+    print(f'shape of xtrain after reshape is {training_data.shape}.')
+
+    #data should be a 4-d tensor where tensor_shape[0] = batch_size, tensor_shape[1] = 1 (grayscale channels), tensor_shape[2] = 16 (height), tensor_shape[3] = 15 (width)
+
+    # batch_size = 32
+    # trainloader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
+    # testloader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
+
+    # model = cnn.ConvolutionalNeuralNetwork()
+
+
+    # plot(data[0][0])
+    # plot(data[1][0])
+    # plot(data[2][0])
+    # plot(data[3][0])
+    # plot(data[4][0])
+    # plot(data[5][0])
+    # plot(data[6][0])
+    # plot(data[7][0])
+    # plot(data[8][0])
+    # plot(data[9][0])
