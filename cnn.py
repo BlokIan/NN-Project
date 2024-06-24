6d16c972b1ca66798e262d13a2703454ca23c33e
BlokIan <i.d.blok@student.rug.nl>
2024-06-24 14:25:48 +0200

    1430 pm update
    Ian: Architecture CNN
    Brian: Data right structure, train-test split, cleanup of code
    Serban: backprop_algorithm

--- cnn.py
+++ cnn.py
@@ -1,12 +1,12 @@
 import os
 import torch
 import torch.optim as optim
-import torch.nn.functional as F
 
 from torch import nn
 from torch.nn import ReLU
 from torch.nn import MaxPool2d
 from torch.nn import Conv2d
+from torch.nn import Linear
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
@@ -14,18 +14,28 @@ from torchvision import transforms
 class ConvolutionalNeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.hidden_stack = nn.Sequential(
+        self.hidden_layer = nn.Sequential(
             # First layer (W*L*D = (15,16,1))
             Conv2d(1, 6, 9, padding=1),
             ReLU(),
             MaxPool2d(2, stride=2, ceil_mode=True),
             # After padding (17, 18, 1), convolution (9, 10, 6), pooling (5, 5, 6))
+
             # Second layer (W*L*D = (5,5,6))
-            Conv2d()
+            Conv2d(6, 16, 2),
+            ReLU(),
+            MaxPool2d(2, stride=2)
+            # After convolution (4,4,16), pooling(2,2,16)
+        )
+        self.classifier_layer = nn.Sequential(
+            # Input from pooling: 64 values (2*2*16 = 64)
+            Linear(64, 32),
+            ReLU(),
+            Linear(32, 10)
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        result = self.hidden_stack(x)
-        return result
+        x = self.hidden_layer(x)
+        x = torch.flatten(x, 1) # Flatten all dimensions except batch
+        logits = self.classifier_layer(x)
+        return logits
--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import sklearn
 import torch
 import torchvision
 import matplotlib.pyplot as plt
-# import cnn
+from cnn import ConvolutionalNeuralNetwork
 
 device = ("cuda")
 print(f"Using {device} device")
 
 
-def get_data(path="mfeat-pix.txt"): 
+def get_data(path="mfeat-pix.txt") -> np.array: 
     # converts data file to array which contains 10 arrays (1 for each number)
     # each list contains 200 images
     # each image is a 15x16 array
@@ -18,7 +18,7 @@ def get_data(path="mfeat-pix.txt"):
     data=data.replace(" ", "")
     array = np.empty((10, 200), dtype=object)
     number = np.empty(200, dtype=object)
-    image = np.empty(15 * 16, dtype=int)
+    image = np.empty(15 * 16, dtype=np.float32)
     image_index = 0
     number_index = 0
     array_index = 0
@@ -30,7 +30,7 @@ def get_data(path="mfeat-pix.txt"):
             else:
                 number[number_index] = image
                 number_index += 1
-            image = np.empty(15 * 16, dtype=int)
+            image = np.empty(15 * 16, dtype=np.float32)
             image_index = 0
             if number_index == 200:
                 array[array_index] = number
@@ -38,41 +38,44 @@ def get_data(path="mfeat-pix.txt"):
                 number = np.empty(200, dtype=object)
                 number_index = 0
         else:
-            image[image_index] = int(value)
+            image[image_index] = float(value) / 6
             image_index += 1
     return array
 
 
-def plot(image):
+def plot(image) -> None:
     plt.imshow(image, cmap='gray', aspect='auto')
     plt.title('Grayscale Image')
     plt.show()
 
 
-if __name__ == "__main__":
-    data = get_data()
+def convert_data_to_input(data):
     training_data = data[:, :100]
     testing_data = data[:, 100:]
+    tensor = 0
+    for number in training_data:
+        for image in number:
+            image = image.reshape([16,15,-1])[:,:,None,:]
+            image = np.moveaxis(image, (2,3), (1,0))
+            if type(tensor) == int:
+                tensor = image
+            else:
+                tensor = np.concatenate((tensor, image), axis = 0)
 
-    training_data = training_data.reshape([15,16,-1])[:,:,None,:]
-    print(f'shape of xtrain after reshape is {training_data.shape}.')
-
-    #data should be a 4-d tensor where tensor_shape[0] = batch_size, tensor_shape[1] = 1 (grayscale channels), tensor_shape[2] = 16 (height), tensor_shape[3] = 15 (width)
-
-    # batch_size = 32
-    # trainloader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
-    # testloader = torch.utils.data.DataLoader(training_data, batch_size=batch_size, shuffle=True)
-
-    # model = cnn.ConvolutionalNeuralNetwork()
+    xtrain = torch.from_numpy(tensor).to(device)
+    return xtrain
 
 
-    # plot(data[0][0])
-    # plot(data[1][0])
-    # plot(data[2][0])
-    # plot(data[3][0])
-    # plot(data[4][0])
-    # plot(data[5][0])
-    # plot(data[6][0])
-    # plot(data[7][0])
-    # plot(data[8][0])
-    # plot(data[9][0])
+def backprop_alg(train_data, test_data, net, epochs, mini_batches=100, gamma=.001, rho=.9):
+        
+    pass
+
+
+if __name__ == "__main__":
+    data = get_data()
+    xtrain = convert_data_to_input(data)
+    # TRY MODEL
+    model = ConvolutionalNeuralNetwork().to(device)
+    result = model(xtrain)
+    print(result)
+    print(np.shape(result))
